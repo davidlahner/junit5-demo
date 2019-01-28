7d61df88eede3d9affac72d540ac8cb90fecbe1f
David Eggerschwiler <mister_x4@hotmail.com>
2019-01-28 14:24:23 +0100

    remove ComparableContract because there is not enough time to show both.

--- interface/src/test/java/implementations/StringTests.java
+++ interface/src/test/java/implementations/StringTests.java
@@ -3,16 +3,11 @@ package implementations;
 import interfaces.ComparableContract;
 import interfaces.EqualsContract;
 
-public class StringTests implements ComparableContract<String>, EqualsContract<String> {
+public class StringTests implements EqualsContract<String> {
 
     @Override
     public String createValue() {
         return "foo";
-    }
-
-    @Override
-    public String createSmallerValue() {
-        return "bar"; // 'b' < 'f' in "foo"
     }
 
     @Override
